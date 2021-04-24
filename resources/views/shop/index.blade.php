2c08dd77a53b8afa551be3ecc0e59d53ee7db190
umran06 <umran0113@gmail.com>
2021-04-24 15:31:26 +0000

    Index Structured & Added CSS File

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -3,3 +3,87 @@
 @section('title')
     Blue Water Café.
 @endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://liverpoolfchqblog.files.wordpress.com/2019/05/0872390c-463a-43d3-9be7-3f46e39b291c.jpeg" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+           <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://liverpoolfchqblog.files.wordpress.com/2019/05/0872390c-463a-43d3-9be7-3f46e39b291c.jpeg" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>   <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://liverpoolfchqblog.files.wordpress.com/2019/05/0872390c-463a-43d3-9be7-3f46e39b291c.jpeg" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>   <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://liverpoolfchqblog.files.wordpress.com/2019/05/0872390c-463a-43d3-9be7-3f46e39b291c.jpeg" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>   <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://liverpoolfchqblog.files.wordpress.com/2019/05/0872390c-463a-43d3-9be7-3f46e39b291c.jpeg" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>   <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://liverpoolfchqblog.files.wordpress.com/2019/05/0872390c-463a-43d3-9be7-3f46e39b291c.jpeg" alt="..." class="img-responsive">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">$12</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
+       

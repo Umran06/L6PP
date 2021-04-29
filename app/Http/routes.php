8d589dd77f970d3d280d8ff167e1af6d1da9abd0
Umran06 <umran0113@gmail.com>
2021-04-29 19:07:28 +0000

    Sign Up Added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,18 @@
 */
 
 Route::get('/', [
-'uses' => 'ProductController@getIndex',
-'as' => 'product.index'
+    'uses' => 'ProductController@getIndex',
+    'as' => 'product.index'
+]);
+
+
+Route::get('/signup', [
+    'uses' => 'UserController@getSignup',
+    'as' => 'user.signup' 
+]);
+
+
+Route::post('/signup', [
+    'uses' => 'UserController@postSignup',
+    'as' => 'user.signup' 
 ]);

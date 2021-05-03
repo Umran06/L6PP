d674aea69ae5a811d59852c53990b71936baf504
Umran06 <umran0113@gmail.com>
2021-05-03 15:47:03 +0000

    Orders Stored in Database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Cart;
 use App\Product;
+use App\Order;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Session;
+use Auth;
 use Stripe\Charge;
 use Stripe\Stripe;
 
@@ -15,32 +17,32 @@ class ProductController extends Controller
 {
     public function getIndex()
     {
-        
-    $products = Product::all();
-    return view ('shop.index', ['products' => $products]);
+        $products = Product::all();
+        return view('shop.index', ['products' => $products]);
     }
-    
+
     public function getAddToCart(Request $request, $id)
-    {   
-    $product = Product::find($id);
-    $oldCart = Session::has('cart') ? Session::get('cart') : null;
-    $cart = new Cart($oldCart);
-    $cart->add($product, $product->id);
-    
-    $request->session()->put('cart', $cart);
-    return redirect()->route('product.index');
+    {
+        $product = Product::find($id);
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $product->id);
+
+        $request->session()->put('cart', $cart);
+        return redirect()->route('product.index');
     }
-    
-    public function getCart(){
-        if (!Session::has ('cart')) {
+
+    public function getCart()
+    {
+        if (!Session::has('cart')) {
             return view('shop.shopping-cart');
         }
         $oldCart = Session::get('cart');
         $cart = new Cart($oldCart);
-        return view ('shop.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
+        return view('shop.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
     }
-    
-       public function getCheckout()
+
+    public function getCheckout()
     {
         if (!Session::has('cart')) {
             return view('shop.shopping-cart');
@@ -61,12 +63,19 @@ class ProductController extends Controller
 
         Stripe::setApiKey('sk_test_51ImK42DAmP0PtNu6eq8pM2inX3n7dWN4QfoJyo5kgZXBVJRhEIN66DiZWbul7wco8TKRtBYSAGMHueDQ71OBjoAR00vNyYwiHP');
         try {
-            Charge::create(array(
+            $charge = Charge::create(array(
                 "amount" => $cart->totalPrice * 100,
-                "currency" => "gbp",
+                "currency" => "usd",
                 "source" => $request->input('stripeToken'), // obtained with Stripe.js
                 "description" => "Test Charge"
             ));
+            $order = new Order();
+            $order->cart = serialize($cart);
+            $order->address = $request->input('address');
+            $order->name = $request->input('name');
+            $order->payment_id = $charge->id;
+            
+            Auth::user()->orders()->save($order);
         } catch (\Exception $e) {
             return redirect()->route('checkout')->with('error', $e->getMessage());
         }
@@ -75,6 +84,3 @@ class ProductController extends Controller
         return redirect()->route('product.index')->with('success', 'Successfully purchased products!');
     }
 }
-
-
-
